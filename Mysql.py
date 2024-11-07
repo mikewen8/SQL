0e46908b93af6333d38c2142eb686be829f9989d
unknown <mikecwen@gmail.com>
2024-11-07 14:46:40 -0800

    changes

---
+++ Mysql.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import mysql.connector
+
+
+print(pd.__version__)
+print(np.__version__)
+
---
+++ firstcpp.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    std::cout<<"this is my first c++ program";
+
+    return 0;
+}
